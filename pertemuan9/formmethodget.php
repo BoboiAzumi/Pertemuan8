155fb52689972ba298e4dafd0c12ce22cd58abbe
BoboiAzumi <untukherokudeploy@gmail.com>
2023-11-10 09:53:38 +0700

    Update Tugasnya

--- pertemuan9/formmethodget.php
+++ pertemuan9/formmethodget.php
@@ -5,5 +5,10 @@
             E-mail : <input type="text" name="email"><br>
             <input type="submit" value="kirim">
         </form>
+        <form method="get" action="kirimmethod_get.php">
+            Nama: <input type="text" name="nama"><br>
+            E-mail : <input type="text" name="email"><br>
+            <input type="submit" value="kirim">
+        </form>
     </body>
 </html>
